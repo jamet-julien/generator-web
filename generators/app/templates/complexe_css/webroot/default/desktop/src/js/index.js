34223536460985fabd421efd4cb5001a5f6b5fd0
julien jamet <jamet.julien@gmail.com>
2017-03-17 18:01:48 +0100

    Mise en place somainere du script de worker

--- generators/app/templates/complexe_css/webroot/default/desktop/src/js/index.js
+++ generators/app/templates/complexe_css/webroot/default/desktop/src/js/index.js
@@ -3,3 +3,43 @@ import test from './class/test.js';
 var oTest = new test();
 
 alert( oTest.nom);
+
+
+/******************************************************************
+                      _        __        __         _
+  ___  ___ _ ____   _(_) ___ __\ \      / /__  _ __| | _____ _ __
+ / __|/ _ \ '__\ \ / / |/ __/ _ \ \ /\ / / _ \| '__| |/ / _ \ '__|
+ \__ \  __/ |   \ V /| | (_|  __/\ V  V / (_) | |  |   <  __/ |
+ |___/\___|_|    \_/ |_|\___\___| \_/\_/ \___/|_|  |_|\_\___|_|
+
+******************************************************************/
+
+if ( 'serviceWorker' in navigator ) {
+
+  navigator.serviceWorker.register('sw.js').then( function(registration) {
+
+    registration.onupdatefound = function() {
+
+        if ( navigator.serviceWorker.controller) {
+          var installingWorker = registration.installing;
+
+          installingWorker.onstatechange = function() {
+
+            switch (installingWorker.state) {
+
+              case 'installed':
+                // mise en cache OK
+                break;
+
+              case 'redundant':
+              default:
+                // Service est devenu redondant.
+            }
+          };
+      }
+    };
+
+  }).catch(function(e) {
+    // pas trouvé le worker
+  });
+}
--- generators/app/templates/simple/js/index.js
+++ generators/app/templates/simple/js/index.js
@@ -1,3 +1,7 @@
 ( function(){
 
+  if ( 'serviceWorker' in navigator ) {
+    navigator.serviceWorker.register('sw.js');
+  }
+
 })();
