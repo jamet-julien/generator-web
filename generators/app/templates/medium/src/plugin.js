6e84505168732990aa502efae0d289a8f3891868
julien jamet <jamet.julien@gmail.com>
2017-09-15 10:04:11 +0200

    Update PLugin JS template

--- generators/app/templates/medium/src/plugin.js
+++ generators/app/templates/medium/src/plugin.js
@@ -1,4 +1,15 @@
+(function (global, factory) {
 
-class Plugin{
+    if (typeof define === "function" && define.amd) define( factory);
+    else if (typeof module === "object") module.exports = factory();
+    else global.pluginName = factory();
 
-}
+}( this, function () {
+    "use strict";
+
+    var Plugin = function(){
+      
+    };
+
+    return Plugin;
+}));
